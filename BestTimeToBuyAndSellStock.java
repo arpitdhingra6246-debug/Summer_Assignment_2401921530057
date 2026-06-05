e041752870e29f1c1efabdd54d95ef83b57a8c5b
Arpit Dhingra <arpitdhingra6246@gmail.com>
2026-06-05 18:42:56 +0530

    Day 1 - Best Time To Buy And Sell Stock

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minimumPrice = prices[0];
+        int maximumProfit = 0;
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < minimumPrice) {
+                minimumPrice = prices[i];
+            }
+             int currentProfit = prices[i] - minimumPrice;
+            if(currentProfit > maximumProfit) {
+                maximumProfit = currentProfit;
+            }
+        }
+        return maximumProfit;
+    }
+}
