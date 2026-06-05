727e2e65ef06a6b699c128edd05d86b85e885645
Arpit Dhingra <arpitdhingra6246@gmail.com>
2026-06-05 18:33:08 +0530

    Day 1 - Remove Duplicates

---
+++ Remove Duplicates from Sorted Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int uniqueIndex = 0;
+        for(int currentIndex = 1;currentIndex<nums.length;currentIndex++){
+            if(nums[currentIndex] !=nums[uniqueIndex]){
+                uniqueIndex++;
+                nums[uniqueIndex] = nums [currentIndex];
+            }
+        }
+        return uniqueIndex + 1;
+    }
+}
