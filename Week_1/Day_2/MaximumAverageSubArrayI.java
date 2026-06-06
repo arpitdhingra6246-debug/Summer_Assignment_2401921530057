31651583f1b2f4fd6b090a88f3c34fb7f65b3006
Arpit Dhingra <arpitdhingra6246@gmail.com>
2026-06-06 16:09:47 +0530

    Update MaximumAverageSubArrayI.java

--- Week_1/Day_2/MaximumAverageSubArrayI.java
+++ Week_1/Day_2/MaximumAverageSubArrayI.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int currentSum = 0;
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
+        int maximumSum = currentSum;
+        for (int i = k; ci < nums.length; i++) {
+            currentSum = currentSum - nums[i - k] + nums[i];
+            if (currentSum > maximumSum) {
+                maximumSum = currentSum;
+            }
+        }
+        return (double) maximumSum / k;
+    }
+}
