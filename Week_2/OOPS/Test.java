f0e9d26d73449c8b7b2b13023cd3fb33c73b9e6a
Arpit Dhingra <arpitdhingra6246@gmail.com>
2026-07-19 17:45:18 +0530

    Create Test.java

---
+++ Week_2/OOPS/Test.java
@@ -0,0 +1,17 @@
+package live;
+import music.playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+public class Test{
+  public static void main(String[]args){
+    Veena veena= new Veena();
+    veena.play();
+    Saxophone saxophone = new Saxophone();
+    saxophone.play();
+Playable p;
+    p = veena;
+    p.play();
+    p = saxophone;
+    p.play();
+  }
+}
