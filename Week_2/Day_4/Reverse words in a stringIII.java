5ee2b60a24cfd2ce3b4c8d199dbf52ce7b20dc5c
Arpit Dhingra <arpitdhingra6246@gmail.com>
2026-07-19 17:16:52 +0530

    Create Reverse words in a stringIII.java

---
+++ Week_2/Day_4/Reverse words in a stringIII.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        String[] words = s.split(" ");
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+
+            String rev = "";
+
+            for (int j = words[i].length() - 1; j >= 0; j--) {
+                rev += words[i].charAt(j);
+            }
+
+            result += rev;
+
+            if (i != words.length - 1) {
+                result += " ";
+            }
+        }
+
+        return result;
+    }
+}
