f95efd40dbb3a4b2f52f4a00e4e23384bbbd4567
Arpit Dhingra <arpitdhingra6246@gmail.com>
2026-07-19 20:03:59 +0530

    Create validatebinarysearchtree.java

---
+++ Week_4/Day_2/validatebinarysearchtree.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return check(root, null, null);
+    }
+    public boolean check(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.val <= min) {
+            return false;
+        }
+        if (max != null && root.val >= max) {
+            return false;
+        }
+        return check(root.left, min, root.val) &&
+               check(root.right, root.val, max);
+    }
+}
